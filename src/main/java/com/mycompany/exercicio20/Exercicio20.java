0e4dee62aba84ddf1bae2bcda83804d9170b3bb1
daros <daros@EsterMariani>
2025-04-09 18:35:12 -0300

    Inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio20/Exercicio20.java
@@ -0,0 +1,14 @@
+package com.mycompany.exercicio20;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio20 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            JOptionPane.showMessageDialog(null, "Número: " + i);
+        }
+
+    }
+}
